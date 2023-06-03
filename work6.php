cf586ed03c34213cdd99abb9a90fd97a4be6970e
Michael Permejo <itsmichaeell@gmail.com>
2023-06-03 22:21:39 +0800

    work6 updated

---
+++ work6.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Portfolio website complete</title>
+        <link rel="stylesheet" href="assets/css/articles.css">
+    </head>
+
+    <body>
+        <article>
+            <img src="assets/img/work6.jpeg">
+
+            <h2 class="blueText thickText biggerText">CICS Week Non-Commissioned Photoshoot</h2>
+
+            <p class="mediumText"></p>
+        </article>
+
+        <footer class="whiteText">
+            <p class="bigText thickText"></p>
+            <p class="thickText">&#169; FIFTY FIFTY. All rights reserved</p>
+        </footer>
+    </body>
+</html>
