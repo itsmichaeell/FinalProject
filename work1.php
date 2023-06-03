ae9f86c12916a82689feea51a48d26016e2b397e
xpriii <127372636+xpriii@users.noreply.github.com>
2023-06-03 22:06:16 +0800

    Add files via upload

---
+++ work1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Portfolio website complete</title>
+        <link rel="stylesheet" href="assets/css/articles.css">
+    </head>
+
+    <body>
+        <article>
+            <img src="assets/img/work1.jpeg">
+
+            <h2 class="blueText thickText biggerText">Batch 2019 Photoshoot</h2>
+
+            <p class="mediumText">One of our first major freelancing projects in our first year in the industry. We were recommended as we have 0% margin at the time, the project was a 2-day photoshoot as the development of the photos happened the next day.</p>
+        </article>
+
+        <footer class="whiteText">
+            <p class="bigText thickText">50/50 PHOTOGRAPHY</p>
+            <p class="thickText">&#169; FIFTY FIFTY. All rights reserved</p>
+        </footer>
+    </body>
+</html>
