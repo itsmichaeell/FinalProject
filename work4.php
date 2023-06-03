1e271b632b36e6ac72141c269f15d7e2d10d2efa
Michael Permejo <itsmichaeell@gmail.com>
2023-06-03 22:18:26 +0800

    work4 updated

---
+++ work4.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Portfolio website complete</title>
+        <link rel="stylesheet" href="assets/css/articles.css">
+    </head>
+
+    <body>
+        <article>
+            <img src="assets/img/work4.jpeg">
+
+            <h2 class="blueText thickText biggerText"></h2>
+
+            <p class="mediumText"></p>
+        </article>
+
+        <footer class="whiteText">
+            <p class="bigText thickText">50/50 PHOTOGRAPHY</p>
+            <p class="thickText">&#169; FIFTY FIFTY. All rights reserved</p>
+        </footer>
+    </body>
+</html>
