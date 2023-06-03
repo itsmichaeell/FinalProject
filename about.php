cdddb163aa6ab3eab6fefde7794b2b3f286f36f3
Michael Permejo <itsmichaeell@gmail.com>
2023-06-03 21:50:42 +0800

    about updated

---
+++ about.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Portfolio website complete</title>
+        <link rel="stylesheet" href="assets/css/articles.css">
+    </head>
+
+    <body>
+        <article>
+            <img src="assets/img/about.webp">
+
+            <h2 class="blueText thickText biggerText">About Us</h2>
+
+            <p class="mediumText">Photography is a small group of photography enthusiasts that operates in the locality of Marinduque mainly in the vicinity of Boac. Started as a group hobby, they entered the freelancing industry with 0% margin. They specialized in wedding photography, graduation photography, documentary, and live coverages for Facebook livestreaming.</p>
+        </article>
+
+        <footer class="whiteText">
+            <p class="bigText thickText">50/50 PHOTOGRAPHY</p>
+            <p class="thickText">&#169; FIFTY FIFTY. All rights reserved</p>
+        </footer>
+        
+        <!--===== SCROLL REVEAL =====-->
+        <script src="https://unpkg.com/scrollreveal"></script>
+
+        <!--===== MAIN JS =====-->
+        <script src="assets/js/main.js"></script>
+    </body>
+</html>
